e92f23638c619083bb30291f8a00c2f9fe644285
liucong <1679421736@qq.com>
2022-07-26 17:06:36 +0800

    second commit

---
+++ src/main/java/com/seecen/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.seecen.git;
+
+/**
+ * @author : liucong
+ * @date : 17:05 2022/7/26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
